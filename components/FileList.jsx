9cd18da0a4442c5df95efb1a3e9cc99413cb112d
Luan <89736491+Luanigi@users.noreply.github.com>
2024-11-09 22:44:42 +0100

    updejjt

--- components/FileList.jsx
+++ components/FileList.jsx
@@ -4,8 +4,8 @@ export default function FileList({ files, playFile, deleteFile }) {
   return (
     <ul className='bg-zinc-700 rounded grid gap-5 md:grid-cols-2 grid-cols-1 mt-10 p-5'>
         {files.map((file, index) => (
-          <li key={index} className='m-auto bg-zinc-600 rounded-lg border-white border-2 p-2'>
-            <span className='text-xl text-white '>{file.name.slice(0, -4)}</span>
+          <li key={index} className='m-auto bg-zinc-600 rounded-lg border-white border-2 p-2 w-full'>
+            <span className='text-lg text-white '>{file.name.slice(0, -4)}</span>
             <br />
             <button onClick={() => playFile(file)}><FaPlay /></button>
             <button onClick={() => deleteFile(file.name)}><FaTrashAlt /></button>
