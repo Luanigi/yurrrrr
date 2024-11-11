dbc31e9ecd27ff09148a10772a5a7d0915898cc8
Luan <89736491+Luanigi@users.noreply.github.com>
2024-11-11 17:13:44 +0100

    desajjjnnn fixxx ~ animation on scroll and button size

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import FileUpload from '../components/FileUpload';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+
+  useEffect(() => {
+    AOS.init({
+         duration: 800,
+         once: false,
+       })
+ }, [])
+
   return (
     <div className="App">
       <FileUpload />
--- components/FileList.jsx
+++ components/FileList.jsx
@@ -4,9 +4,9 @@ export default function FileList({ files, playFile, deleteFile }) {
   return (
     <ul className='bg-zinc-700 rounded grid gap-5 md:grid-cols-2 grid-cols-1 mt-10 p-5'>
         {files.map((file, index) => (
-          <li key={index} className='m-auto bg-zinc-600 rounded-lg border-white border-2 p-2 w-full'>
+          <li key={index} className='m-auto bg-zinc-600 rounded-lg border-white border-2 p-2 w-full' data-aos="zoom-in">
             <span className='text-lg text-white '>{file.name.slice(0, -4)}</span>
-            <br />
+            {/* <br /> */}
             <button onClick={() => playFile(file)}><FaPlay /></button>
             <button onClick={() => deleteFile(file.name)}><FaTrashAlt /></button>
           </li>
